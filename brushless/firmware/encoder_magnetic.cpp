c540396185c4564a3f564ee891460623db1d33aa
Gregwar <g.passault@gmail.com>
2018-02-08 21:34:45 +0100

    Removing useless variable

--- brushless/firmware/encoder_magnetic.cpp
+++ brushless/firmware/encoder_magnetic.cpp
@@ -106,7 +106,6 @@ bool encoder_read()
     encoder_delta_pos++;
 
     if (encoder_delta_pos >= 8) {
-        uint16_t old_magnetic_value = magnetic_value;
         encoder_deltas /= 8;
         magnetic_value = (magnetic_value + encoder_deltas + 0x4000)%(0x4000);
 
