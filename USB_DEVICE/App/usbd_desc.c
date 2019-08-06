cca06bcd549db171ceca2ea2e15b26e730d15470
Adrien Boussicault <boussica@labri.fr>
2019-08-06 18:56:50 +0200

    Define Vendor ID and Product ID for USB

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -63,11 +63,11 @@
   * @{
   */
 
-#define USBD_VID     1155
+#define USBD_VID     0xF123
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
+#define USBD_MANUFACTURER_STRING     "OpenHandicap"
+#define USBD_PID_FS     0x4567
+#define USBD_PRODUCT_STRING_FS     "Brushless Driver"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
 #define USBD_INTERFACE_STRING_FS     "CDC Interface"
 
